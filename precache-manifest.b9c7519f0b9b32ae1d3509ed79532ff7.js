a96be16bc06d908dfa3dfb93fa2fa58052d2c579
Roger <rmartinez5926.rm@gmail.com>
2020-05-07 18:48:39 -0400

    Updates

---
+++ precache-manifest.b9c7519f0b9b32ae1d3509ed79532ff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d195101c6a18f858223ad6ec3b9404",
+    "url": "/Employee_Management/index.html"
+  },
+  {
+    "revision": "ed394e69af9d0f5d65f9",
+    "url": "/Employee_Management/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "90e51a4166a96014693e",
+    "url": "/Employee_Management/static/js/2.c927648d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee_Management/static/js/2.c927648d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed394e69af9d0f5d65f9",
+    "url": "/Employee_Management/static/js/main.e7c69190.chunk.js"
+  },
+  {
+    "revision": "f6ca9ae7cbc9b771889f",
+    "url": "/Employee_Management/static/js/runtime-main.dcbc5cd4.js"
+  }
+]);
